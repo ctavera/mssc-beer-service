bda1ec93190bbd4301841d9f5123ed985eab8baa
christian.tavera <christian.tavera@grupoargos.com.mx>
2020-12-11 16:09:18 -0500

    Adding beer repository. Closes #6

---
+++ src/main/java/da/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package da.springframework.msscbeerservice.repositories;
+
+import da.springframework.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
