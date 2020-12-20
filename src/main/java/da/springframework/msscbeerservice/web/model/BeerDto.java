9c63cbb0c735d426a5ab7c262950121d0bacf31b
christian.tavera <christian.tavera@grupoargos.com.mx>
2020-12-19 20:54:10 -0500

    Adding Jackson Config for BeerDto. Closes #13

--- src/main/java/da/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/da/springframework/msscbeerservice/web/model/BeerDto.java
@@ -1,6 +1,7 @@
 package da.springframework.msscbeerservice.web.model;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,9 +28,11 @@ public class BeerDto {
     private Integer version;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
@@ -42,6 +45,7 @@ public class BeerDto {
     @NotNull
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
