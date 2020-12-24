5d764827d5ba4cf5a90977858167b538715e6400
christian.tavera <christian.tavera@grupoargos.com.mx>
2020-12-23 23:11:29 -0500

    127. Data Initialization.

--- src/main/java/da/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/da/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -3,11 +3,10 @@ package da.springframework.msscbeerservice.bootstrap;
 import da.springframework.msscbeerservice.domain.Beer;
 import da.springframework.msscbeerservice.repositories.BeerRepository;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
-@Component
+//@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
