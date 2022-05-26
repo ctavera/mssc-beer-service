372840810db98a81b566f3f37b0ddd73f6535761
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-05-26 12:30:36 -0500

    Configure JMS. Closes #24

---
+++ src/main/java/da/springframework/msscbeerservice/config/JmsConfig.java
@@ -0,0 +1,21 @@
+package da.springframework.msscbeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    //Serialize message content to json using TextMessage
+    @Bean
+    public MessageConverter meesageConverter(){
+        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+        messageConverter.setTargetType(MessageType.TEXT);
+        messageConverter.setTypeIdPropertyName("_type");
+
+        return messageConverter;
+    }
+}
