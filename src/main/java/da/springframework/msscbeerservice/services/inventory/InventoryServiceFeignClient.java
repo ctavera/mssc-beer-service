6603acc4a83d9401f43e243aa9b9aaac74448538
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-06-28 11:07:00 -0500

    291. Configure Feign Client for HTTP Basic Authentication. Closes #46

--- src/main/java/da/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
+++ src/main/java/da/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
@@ -1,5 +1,6 @@
 package da.springframework.msscbeerservice.services.inventory;
 
+import da.springframework.msscbeerservice.config.FeignClientConfig;
 import da.springframework.msscbeerservice.services.inventory.model.BeerInventoryDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "inventory-service", fallback = InventoryServiceFeignClientFailover.class)
+@FeignClient(name = "inventory-service", fallback = InventoryServiceFeignClientFailover.class, configuration = FeignClientConfig.class)
 public interface InventoryServiceFeignClient {
 
     @RequestMapping(method = RequestMethod.GET, value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
